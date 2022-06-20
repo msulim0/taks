919d4c5244a95740e229937dcaee68d92060ce39
msulim0 <107648688+msulim0@users.noreply.github.com>
2022-06-20 14:09:54 +0300

    Add files via upload

--- task3/task3.R
+++ task3/task3.R
@@ -6,24 +6,22 @@ library(tidyverse)
 total_pr <- select(task3, invoiceno, iInvoicedate, unitprice, quantity) %>%
   group_by(invoiceno, iInvoicedate) %>%
   mutate(amount_or = quantity * unitprice) %>%
-  summarize(tot_am = sum(amount_or)) %>%
+  summarize(tot_am_by_invoice = sum(amount_or)) %>%
   arrange(iInvoicedate)
-head(total_pr, 15)
 
-
-# total amount and quantity of each customer's order
-by_amount <- select(task3, customerid, quantity, unitprice) %>%
-  group_by(customerid) %>%
+# total quantity of each customer's order
+by_amount <- select(task3, invoiceno, customerid, quantity, unitprice) %>%
+  group_by(invoiceno, customerid) %>%
   mutate(amount = quantity * unitprice) %>%
-  summarize(total_order = n(),
-            tot_am = sum(amount))
-head(by_amount, 15)
+  summarize(total_quantity_by_invoice = n())
 
 # customers order by date
 by_date <- select(task3, customerid, quantity, unitprice, iInvoicedate) %>%
-  group_by(iInvoicedate, customerid) %>%
+  group_by(customerid) %>%
   mutate(amount = quantity * unitprice) %>%
-  summarize(total_order = n(),
-            tot_am = sum(amount)) %>%
+  summarize(tot_am_by_cm = sum(amount)) %>%
   arrange(customerid)
-head(by_date, 15)
+
+# merge
+df = merge(x=total_pr,y=by_amount,by="invoiceno")
+result_of_task = merge(x=df,y=by_date,by="customerid")
